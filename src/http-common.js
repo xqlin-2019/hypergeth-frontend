c5c435bbcb2cf3736b43b66be22c15c2505e9079
johncheong1234 <john.cheong.2019@smu.edu.sg>
2021-09-27 11:27:34 +0800

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://3.229.199.112:3000/",
+  baseURL: "http://localhost:3000/",
   headers: {
     "Content-type": "application/json"
   }
